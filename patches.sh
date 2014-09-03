4023db7fff585c221438b6dcffb37cbe579a1b6e
Joseph Riches <josephriches@gmail.com>
2014-09-03 10:22:57 +0100

    manifest: remove cherry-picks these are merged

--- patches.sh
+++ patches.sh
@@ -5,24 +5,6 @@ if [ "${android}" = "" ]; then
         android=~/android/system
 fi
 
-# hwc: rotator is not supported on msm7x27a
-cherries+=(69274)
-
-# libstagefright: Add support for custom LPA buffer size in legacy LPAPlayer
-cherries+=(69272)
-
-# libstagefright: Enable meta mode for 7x27a video encoder
-cherries+=(69271)
-
-# libstagefright: refactor Legacy LPA makefile
-cherries+=(69270)
-
-# display: Cover NO_IOMMU for msm7x30/msm7x27a too
-cherries+=(66211)
-
-# media-caf: Fix building for msm7x27a
-cherries+=(69273)
-
 ${android}/build/tools/repopick.py -b ${cherries[@]}
 
 # Apply patches from tamsui-common
